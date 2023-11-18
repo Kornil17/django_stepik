5e3d7b9e8162073cfdf7a5285442d79a08a0b703
Kornil17 <dimitra4@mail.ru>
2023-11-18 15:17:21 +0300

    add test file for horoscope

--- first_projects/horoscope/tests.py
+++ first_projects/horoscope/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestHoroscope(TestCase):
+
+    def test_index(self):
+        response = self.client.get('/horoscope/')
+        self.assertEqual(response.status_code, 200)
+    def test_air(self):
+        response = self.client.get('/horoscope/type')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Air", response.content.decode())
